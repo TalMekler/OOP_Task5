96884b05760aea7081a985673a7b11b24a00e669
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-31 23:39:47 +0300

    Finish except 1 CTOR

--- MammalsFish.cpp
+++ MammalsFish.cpp
@@ -1,1 +1,14 @@
 #include "MammalsFish.h"
+
+MammalsFish::MammalsFish() :Mammals(), Fish() {};
+
+MammalsFish::MammalsFish(const char *color, int childs, float avgLifetime, float preg, float milk, int fin, int gills) : Mammals(color, childs, avgLifetime,preg, milk),
+                                                                                                                         Fish(color, childs, avgLifetime,fin, gills) {};
+
+MammalsFish::MammalsFish(ifstream &in_file) : Mammals(in_file) {
+
+}
+
+MammalsFish::~MammalsFish() {
+
+}
