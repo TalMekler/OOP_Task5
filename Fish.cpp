cfa0d6ea2b4e4f6e0922b2f6777604baad839036
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-31 23:36:30 +0300

    Finish except 1 CTOR

--- Fish.cpp
+++ Fish.cpp
@@ -1,1 +1,23 @@
 #include "Fish.h"
+
+Fish::Fish() : Animal(), m_finCount(0), m_gillsCount(0) {};
+
+Fish::Fish(const char *color, int childs, float avgLifetime, int fin, int gills) : Animal(color, childs, avgLifetime),
+                                                                                   m_gillsCount(gills),
+                                                                                   m_finCount(fin) {};
+
+Fish::Fish(ifstream &in_file) : Animal(in_file) {
+
+}
+
+Fish::~Fish() {
+
+}
+
+int Fish::GetFinCount() const {
+    return m_finCount;
+}
+
+int Fish::GetGillsCount() const {
+    return m_gillsCount;
+}
