e3803d18f8052e820db3ccea36992e1c6534036f
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-31 23:33:19 +0300

    Finish except 1 CTOR

--- Mammals.cpp
+++ Mammals.cpp
@@ -1,1 +1,21 @@
 #include "Mammals.h"
+
+Mammals::Mammals() : Animal(), m_milkLiters(0), m_pregnancyTime(0) {};
+
+Mammals::Mammals(const char *color, int childs, float avgLifetime, float preg, float milk) : Animal(color, childs, avgLifetime), m_pregnancyTime(preg), m_milkLiters(milk) {};
+
+Mammals::Mammals(ifstream &in_file) : Animal(in_file) {
+
+}
+
+Mammals::~Mammals() {
+
+}
+
+float Mammals::GetPregnanceTime() const {
+    return m_pregnancyTime;
+}
+
+float Mammals::GetMilk() const {
+    return m_milkLiters;
+}
