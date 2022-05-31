56dcbb7bd0201f5b589a5e3b14fe1a2f79e2068c
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-31 23:53:23 +0300

    Finish except 1 Ctor

--- Birds.cpp
+++ Birds.cpp
@@ -1,2 +1,17 @@
 #include "Birds.h"
 
+Birds::Birds():Animal() {};
+
+Birds::Birds(const char *color, int childs, float avgLifetime, float incubation):Animal(color, childs, avgLifetime), m_incubationTime(incubation) {};
+
+Birds::Birds(ifstream &in_file) : Animal(in_file) {
+
+}
+
+Birds::~Birds() {
+
+}
+
+float Birds::GetIncubationTime() const {
+    return m_incubationTime;
+}
