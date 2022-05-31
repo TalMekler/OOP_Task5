afe6b98264465cd77abf6e2286a66df79d17de23
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-31 23:49:57 +0300

    Finish except 1 CTOR

--- Horse.cpp
+++ Horse.cpp
@@ -1,1 +1,19 @@
 #include "Horse.h"
+
+Horse::Horse() : Mammals(), m_type(nullptr) {};
+
+Horse::Horse(const char *color, int childs, float avgLifetime, float preg, float milk, const char *type): Mammals(color, childs, avgLifetime, preg, milk) {
+    m_type = strdup(type);
+}
+
+Horse::Horse(ifstream &in_file) : Mammals(in_file) {
+
+}
+
+Horse::~Horse() {
+    delete[] m_type;
+}
+
+const char *Horse::GetType() const {
+    return m_type;
+}
