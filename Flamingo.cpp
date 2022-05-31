d72259f3b247492b317962406e5c6a139e1a5b44
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-31 23:52:11 +0300

    Finish except 1 Ctor

--- Flamingo.cpp
+++ Flamingo.cpp
@@ -1,1 +1,17 @@
 #include "Flamingo.h"
+
+Flamingo::Flamingo(): Birds(), m_avgHeight(0) {};
+
+Flamingo::Flamingo(const char *color, int childs, float avgLifetime, float incubation, float avgHeight):Birds(color, childs,avgLifetime, incubation), m_avgHeight(avgLifetime) {};
+
+Flamingo::Flamingo(ifstream &in_file) : Birds(in_file) {
+
+}
+
+Flamingo::~Flamingo() {
+
+}
+
+float Flamingo::GetHeight() const {
+    return m_avgHeight;
+}
