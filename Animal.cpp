8af6100da0bf43b149ff87cbf130405f5d3d1cef
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-31 23:29:57 +0300

    CTOR's (except one), DTOR, get methods

--- Animal.cpp
+++ Animal.cpp
@@ -1,2 +1,33 @@
 #include "Animal.h"
 
+Animal::Animal() {
+    m_color = strdup("GRAY");
+    m_childCount = 0;
+    m_avgLifetime = 0;
+}
+
+Animal::Animal(const char *color, int childs, float avgLifetime) {
+    m_color = strdup(color);
+    m_childCount = childs;
+    m_avgLifetime = avgLifetime;
+}
+
+Animal::Animal(ifstream &in_file) {
+
+}
+
+Animal::~Animal() {
+    delete[] m_color;
+}
+
+const char *Animal::GetColor() const {
+    return m_color;
+}
+
+int Animal::GetChildCount() const {
+    return m_childCount;
+}
+
+float Animal::GetLifetime() const {
+    return m_avgLifetime;
+}
