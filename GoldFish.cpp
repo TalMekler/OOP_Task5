e22d5ecf7ad91bb368103c1e0a35fb312c9b77ec
Tal Mekler <Tal.mekler11@gmail.com>
2022-06-01 00:03:50 +0300

    Finish except 1 Ctor

--- GoldFish.cpp
+++ GoldFish.cpp
@@ -1,1 +1,23 @@
 #include "GoldFish.h"
+
+GoldFish::GoldFish() : MammalsFish(), m_avgWeight(0), m_avgLength(0) {};
+
+GoldFish::GoldFish(const char *color, int childs, float avgLifetime, float preg, float milk, int fin, int gills,
+                   float avgW, float avgL) : MammalsFish(color, childs, avgLifetime, preg, milk, fin, gills),
+                                             m_avgLength(avgL), m_avgWeight(avgW) {};
+
+GoldFish::GoldFish(ifstream &in_file) : MammalsFish(in_file) {
+
+}
+
+GoldFish::~GoldFish() {
+
+}
+
+float GoldFish::GetWeight() const {
+    return m_avgWeight;
+}
+
+float GoldFish::GetLength() const {
+    return m_avgLength;
+}
