a9ee4a031e7351d23e961349cb7aa33b1376e0f8
Tal Mekler <Tal.mekler11@gmail.com>
2022-06-01 18:43:22 +0300

    Save & Load methods

--- Animal.cpp
+++ Animal.cpp
@@ -7,13 +7,13 @@ Animal::Animal() {
 }
 
 Animal::Animal(const char *color, int childs, float avgLifetime) {
-    m_color = strdup(color);
+    setColor(color);
     m_childCount = childs;
     m_avgLifetime = avgLifetime;
 }
 
-Animal::Animal(ifstream &in_file) {
-
+Animal::Animal(ifstream &in_file) : m_color(nullptr) {
+    LoadBin(in_file);
 }
 
 Animal::~Animal() {
@@ -31,3 +31,39 @@ int Animal::GetChildCount() const {
 float Animal::GetLifetime() const {
     return m_avgLifetime;
 }
+
+void Animal::setColor(const char* color) {
+    delete[] m_color;
+    m_color = strdup(color);
+}
+
+void Animal::Save(ofstream &out_file) const {
+    out_file << m_color << " " << m_childCount << " " << m_avgLifetime << endl;
+}
+
+void Animal::Load(ifstream &in_file) {
+    char buff[200];
+    in_file >> buff;
+    setColor(buff);
+    in_file >> m_childCount;
+    in_file >> m_avgLifetime;
+}
+
+void Animal::SaveBin(ofstream &out_file) const {
+    int len = strlen(m_color);
+    out_file.write((char*)&len, sizeof(len));
+    out_file.write(m_color, len);
+    out_file.write((char*)&m_childCount, sizeof(m_childCount));
+    out_file.write((char*)&m_avgLifetime, sizeof(m_avgLifetime));
+}
+
+void Animal::LoadBin(ifstream &in_file) {
+    int len;
+    char buff[200];
+    in_file.read((char*)&len, sizeof(len));
+    in_file.read(buff, len);
+    setColor(buff);
+    in_file.read((char*)&m_childCount, sizeof(m_childCount));
+    in_file.read((char*)&m_avgLifetime, sizeof(m_avgLifetime));
+}
+
--- Animal.h
+++ Animal.h
@@ -15,6 +15,7 @@ class Animal
 protected:
 	Animal();//set the default color to GRAY and other params to 0
 	Animal( const char* color, int childs, float avgLifetime );//init the Animal with a given attributes
+//    Animal(Animal& a); // copy CTOR
 	Animal( ifstream& in_file );//init the Animal from a binary file
 
 public:
@@ -24,11 +25,22 @@ public:
 	const char* GetColor() const;//return the color of the animal
 	int			GetChildCount() const;//return the child count of the animal
 	float		GetLifetime() const;//return the life time of the animal
+    void setColor(const char* color);
+
+    virtual void Save(ofstream& out_file) const;
+    virtual void Load(ifstream& in_file);
+    virtual void SaveBin(ofstream& out_file) const;
+    virtual void LoadBin(ifstream& in_file);
+
+    virtual void saveAddition(ofstream& out_file)const = 0;
+    virtual void saveAdditionBin(ofstream& out_file)const = 0;
+    virtual void loadAddition(ifstream& in_file) = 0;
+    virtual void loadAdditionBin(ifstream& in_file) = 0;
 
 protected:
 	char*	m_color;
 	int		m_childCount;
-	float	m_avgLifetime;
+	float 	m_avgLifetime;
 };
 
 #endif // ifndef
