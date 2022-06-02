99d9c98eb394d647f73225b1ddba23c8318a4725
Tal Mekler <Tal.mekler11@gmail.com>
2022-06-02 10:36:56 +0300

    .

--- MammalsFish.cpp
+++ MammalsFish.cpp
@@ -4,14 +4,9 @@ MammalsFish::MammalsFish() :Mammals(), Fish() {};
 
 MammalsFish::MammalsFish(const char *color, int childs, float avgLifetime, float preg, float milk, int fin, int gills) : Mammals(color, childs, avgLifetime,preg, milk),
                                                                                                                          Fish(color, childs, avgLifetime,fin, gills) {};
+MammalsFish::MammalsFish(ifstream &in_file) : Mammals(in_file), Fish(in_file){};
 
-MammalsFish::MammalsFish(ifstream &in_file) : Mammals(in_file), Fish(in_file) {
-
-}
-
-MammalsFish::~MammalsFish() {
-
-}
+MammalsFish::~MammalsFish()=default;
 
 void MammalsFish::Save(ofstream &out_file) const {
     Animal::Save(out_file);
